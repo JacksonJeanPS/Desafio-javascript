38155ca5fe50d3908540119fab65b54fb2e44863
Jackson JP <95863318+JacksonJeanPS@users.noreply.github.com>
2022-10-15 18:52:39 -0300

    Create dragao.js

---
+++ dragao.js
@@ -0,0 +1,17 @@
+
+let input = gets();
+
+let qtdEntradas = parseInt(input);
+let nivelEnergia;
+let N = nivelEnergia
+
+for(let i = 0; i < qtdEntradas; i++){
+    input = gets();
+    nivelEnergia = parseInt(input);
+    
+    if( 8000 >=  nivelEnergia){
+        print ("Inseto!")
+      }else {
+        print ("Mais de 8000!")
+    };
+};
